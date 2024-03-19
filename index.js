932ec11518c858f034fc4eb309d96262c60edff8
bigsteppa05 <habibforderiv05@gmail.com>
2024-03-19 11:56:07 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+    function  mondayWork(action = 'go to the office') {
+        return 'This Monday, I will ' + action .'
+        }
+        
+function  wrapAdjective(adjective = '*') {
+    return function(emphatic) {
+        return `You are ${adjective}${emphatic}${adjective}!`
+    }
+    
+}
+
